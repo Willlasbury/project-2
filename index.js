82e38b64cfb940fe4f7c2661648b2bb61d2375ea
Willlasbury <asburyw229@gmail.com>
2023-05-05 12:42:39 -0700

    added boilerplate code

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const sequelize = require('./config')
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const hbs = exphbs.create({});
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        maxAge:1000*60*60*2
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session(sess))
+
+const allRoutes = require('./controllers')
+app.use(allRoutes)
+const User = require('./models/User')
+sequelize.sync({force:false}).then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`listenin to port PORT!`)
+    })
+})
