285050bbd36a018dfc6339012bed2898154948ec
Willlasbury <asburyw229@gmail.com>
2023-05-05 14:14:03 -0700

    added jawsdb_url

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize')
 require("dotenv").config();
 
 let sequelize;
 
-if (process.env.JAWS_DB) {
-  sequelize = new Sequelize(process.env.JAWS_DB);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
