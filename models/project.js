384a5db926c803c4a207f174dff163e653a4c9f2
Michael Campbell <michaelcampbell@Michaels-MBP.domain>
2023-05-08 09:59:19 -0700

    project.js and project routes

---
+++ models/project.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config');
+
+
+class Project extends Model {}
+
+Project.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+     due_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        validate: {
+            isDate: true,
+        }
+     }
+    },
+    {
+      sequelize,
+    }
+  );
+
+module.exports = Project;
