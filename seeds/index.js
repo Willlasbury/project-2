26278ab045af2141f25690872418598594ae5d7f
Willlasbury <asburyw229@gmail.com>
2023-05-08 13:52:34 -0700

    added one reference for ProjectUsers table

--- seeds/index.js
+++ seeds/index.js
@@ -11,7 +11,14 @@ const startSeedin = async () => {
     const projectData = await Project.bulkCreate(projectSeeds);
     const userData = await User.bulkCreate(userSeeds);
     const taskData = await Task.bulkCreate(taskSeeds);
+    // console.log("userData:", userData)
+    console.log('===\n\n\ntest\n\n\n===')
+    await userData[0].addProjects(1)
 
+    // for (let i = 0; i < userData.length; i++) {
+    //   userData[i].addProject(1)
+      
+    // }
     process.exit(0);
   } catch (err) {
     console.log(err);
