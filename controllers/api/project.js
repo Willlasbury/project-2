37ff72a6e4b65773e89b1344dfa4d5253e1d49b7
Alex Horning <68720206+alexandertbh@users.noreply.github.com>
2023-05-08 14:32:09 -0700

    updating task routes

--- controllers/api/project.js
+++ controllers/api/project.js
@@ -1,85 +1,87 @@
-const router = require('express').Router();
-const {Project, Task } = require('../../models');
+const router = require("express").Router();
+const { Project, Task } = require("../../models");
 
 // get all projects
-  router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   // find all projects
   // be sure to include its associated Category and Tag data
-    try{
-    const projects = await Project.findAll( {
-    include: [{ model: Task}],
+  try {
+    const projects = await Project.findAll({
+      include: [{ model: Task }],
     });
-    if(projects.length <1){
-      return res.status(404).json({ message: 'There are no projects in your database' });
-    }else{
-    res.json(projects);
+    if (projects.length < 1) {
+      return res
+        .status(404)
+        .json({ message: "There are no projects in your database" });
+    } else {
+      res.json(projects);
     }
-    }catch (err){
+  } catch (err) {
     res.status(500).json(err);
+  }
+});
+// get one project
+router.get("/:id", async (req, res) => {
+  // find a single product by its `id`
+  // be sure to include its associated Category and Tag data
+  try {
+    const projectID = await Project.findByPk(req.params.id, {
+      include: [{ model: Task }],
+    });
+
+    if (!projectID) {
+      res.status(404).json({ message: "No project found with that id!" });
+      return;
     }
-  });
-  // get one project
-router.get('/:id', async (req, res) => {
-    // find a single product by its `id`
-    // be sure to include its associated Category and Tag data
-    try {
-      const projectID = await Project.findByPk(req.params.id, {
-        include: [{ model:Task}],
-      });
-  
-      if (!projectID) {
-        res.status(404).json({ message: 'No project found with that id!' });
-        return;
-      }
-  
-      res.status(200).json(projectID);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-  // create new project
-  router.post('/', (req, res) => {
-    Project.create(req.body)
-      .then((newProject) => {
-        res.json(newProject);
-      })
-      .catch((err) => {
-        res.json(err);
-      });
-  });
-  
-  // update project
-  router.put('/:id', (req, res) => {
-    Project.update(
-      {
-        title: req.body.title,
-      },
-      {
-        where: {
-          id: req.params.id,
-        },
-      }
-    )
-      .then((updatedProject) => {
-        res.json(updatedProject);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.json(err);
-      });
-  });
-   // delete a product by its `id` value
-  router.delete('/:id', (req, res) => {
-    Project.destroy({
+
+    res.status(200).json(projectID);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// create new project
+router.post("/", async (req, res) => {
+  try {
+    const dbResponse = await Project.create(req.body);
+    await dbResponse.addProjects(req.body.userId);
+    console.log(dbResponse[0]);
+    res.json(dbResponse);
+  } catch (err) {
+    res.status(500).json({ err: err });
+  }
+});
+
+// update project
+router.put("/:id", (req, res) => {
+  Project.update(
+    {
+      title: req.body.title,
+    },
+    {
       where: {
         id: req.params.id,
       },
+    }
+  )
+    .then((updatedProject) => {
+      res.json(updatedProject);
     })
-    .then((deletedProject)=>{
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+});
+// delete a product by its `id` value
+router.delete("/:id", (req, res) => {
+  Project.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedProject) => {
       res.json(deletedProject);
     })
     .catch((err) => res.json(err));
-  })
-  
-  module.exports = router;
-  
+});
+
+module.exports = router;
