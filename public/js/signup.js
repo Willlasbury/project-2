ad1dd4863ebd8fcb24390c2d4b0d010496010899
Willlasbury <asburyw229@gmail.com>
2023-05-08 19:34:19 -0700

    added password check functionality to sign up

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,33 +1,28 @@
 const signUpForm = document.querySelector("#signup-form");
-const password = document.querySelector('#user-password').value
-const passConfim = document.querySelector('#user-password-confirmation').value
-
-console.log('===\n\n\ntest\n\n\n===')
 
 const checkPassword = (pass1, pass2) => {
-    if (pass1 === pass2){
-        return true
-    } else {
-        return false
-    }
-}
-
-
+  if (pass1 === pass2) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 signUpForm.addEventListener("submit", async (event) => {
-    try {
-
-        console.log("checkPassword(password):", checkPassword(password,passConfim))
-
-
-    
+  try {
+    const password = document.querySelector("#user-password").value;
+    const passConfirm = document.querySelector(
+      "#user-password-confirmation"
+    ).value;
+    if (await checkPassword(password, passConfirm)) {
       event.preventDefault();
+
       const newUser = {
         userName: document.querySelector("#user-name").value,
         email: document.querySelector("#user-email").value,
         password: document.querySelector("#user-password").value,
       };
-  
+
       const response = await fetch("/api/users", {
         method: "POST",
         headers: {
@@ -35,14 +30,18 @@ signUpForm.addEventListener("submit", async (event) => {
         },
         body: JSON.stringify(newUser),
       });
-      console.log("response:", response)
+      console.log("response:", response);
       if (response.ok) {
         location.href = "/";
       } else {
         alert("some error");
       }
-    } catch (err) {
-      console.log("error:", error);
-      alert(err);
+    } else {
+        console.log('===\n\n\ntest\n\n\n===')
+       alert("passwords must be the same");
     }
-  });
+  } catch (err) {
+    console.log("error:", error);
+    alert(err);
+  }
+});
