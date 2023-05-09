b6ee85bcb2871ee33988910450264cdbb67a6ed4
Willlasbury <asburyw229@gmail.com>
2023-05-08 18:47:58 -0700

    readded middleware to public folder

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,14 +1,32 @@
 const signUpForm = document.querySelector("#signup-form");
+const password = document.querySelector('#user-password').value
+const passConfim = document.querySelector('#user-password-confirmation').value
+
+console.log('===\n\n\ntest\n\n\n===')
+
+const checkPassword = (pass1, pass2) => {
+    if (pass1 === pass2){
+        return true
+    } else {
+        return false
+    }
+}
+
+
 
 signUpForm.addEventListener("submit", async (event) => {
     try {
+
+        console.log("checkPassword(password):", checkPassword(password,passConfim))
+
+
+    
       event.preventDefault();
       const newUser = {
         userName: document.querySelector("#user-name").value,
         email: document.querySelector("#user-email").value,
         password: document.querySelector("#user-password").value,
       };
-  
   
       const response = await fetch("/api/users", {
         method: "POST",
