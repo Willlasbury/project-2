4d9011920d243e4ad8dba12661ce304b7a83f623
Alex Horning <68720206+alexandertbh@users.noreply.github.com>
2023-05-11 10:42:50 -0700

    fixing create project and task status function locations

--- public/js/createProject.js
+++ public/js/createProject.js
@@ -36,3 +36,24 @@ for (let i = 0; i < id.length; i++) {
   element.addEventListener("change", updateFunction);
 }
 
+document.querySelector("form").addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const projectObj = {
+    title: document.querySelector("#project").value,
+    due_date: document.querySelector("#due-date").value,
+  };
+  const response = await fetch("/api/projects", {
+    method: "POST",
+    body: JSON.stringify(projectObj),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const projectData = await response.json();
+  const id = projectData.id;
+  if (response.ok) {
+    location.href = `/create_tasks/${id}`;
+  } else {
+    alert("You have an error");
+  }
+});
