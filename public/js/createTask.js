a7c9b114831bec452d17a8dff7077cbd0d3d0817
Alex Horning <68720206+alexandertbh@users.noreply.github.com>
2023-05-12 11:52:01 -0700

    adding formatting for dates and task styles

--- public/js/createTask.js
+++ public/js/createTask.js
@@ -1,26 +1,28 @@
-document.querySelector("form").addEventListener("submit", event=>{
-    event.preventDefault();
-   const projID = document.querySelector("#title").dataset.id;
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const projID = document.querySelector("#title").dataset.id;
 
-    const taskObj = {
-        name: document.querySelector("#task-name").value,
-        due_date: document.querySelector("#due-date").value,
-        description: document.querySelector("#description").value,
-        status: null,
-        ProjectId: projID
+  const taskObj = {
+    name: document.querySelector("#task-name").value,
+    due_date: dayJs(document.querySelector("#due-date").value).format(
+      "MMMM, DD, YYYY"
+    ),
+    description: document.querySelector("#description").value,
+    status: null,
+    ProjectId: projID,
+  };
+  console.log(taskObj);
+  fetch("/api/tasks", {
+    method: "POST",
+    body: JSON.stringify(taskObj),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
+    if (res.ok) {
+      location.href = `/project/${projID}`;
+    } else {
+      alert("You have an error");
     }
-    console.log(taskObj);
-    fetch("/api/tasks", {
-        method: "POST",
-        body: JSON.stringify(taskObj),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }).then(res =>{
-        if(res.ok){
-            location.href = `/project/${projID}`
-        } else{
-            alert("You have an error")
-        }
-    })
-})
+  });
+});
